e0b7cbf5c9253a955ba86f1602a8c41bed4548f2
Muhammad Abdulmalik <hackobansa@gmail.com>
2025-08-28 04:08:40 +0000

    please  homepages not displaying

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/scriptember25',
-  assetPrefix: '/scriptember25/',
   images: {
     unoptimized: true,
     remotePatterns: [
