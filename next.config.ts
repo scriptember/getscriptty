aa27758b1503ef1acc1823895c5a31e440ba8f60
Muhammad Abdulmalik <hackobansa@gmail.com>
2025-08-31 11:28:53 +0000

    push to  getscriptty repo on github

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/scriptember25',
+  basePath: '/getscriptty',
   images: {
     unoptimized: true,
     remotePatterns: [
