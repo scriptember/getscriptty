f666e1fd94b8bc1846991f3d49be5ae4e49ee0e4
Muhammad Abdulmalik <hackobansa@gmail.com>
2025-08-31 11:40:03 +0000

    use relevant files or folder recommended for hosting github pages

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,12 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
