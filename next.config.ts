e9feb7457334a53f1298b2dbee3f88fa95f37197
Muhammad Abdulmalik <hackobansa@gmail.com>
2025-09-01 09:24:51 +0000

    while push to pxxl.app as using github deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/getscriptty',
   images: {
     unoptimized: true,
     remotePatterns: [
